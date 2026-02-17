3d0e394c77785c43c1d45e70acb49006141eef1f
JustinLarry <justinlarrydimaiwat@gmail.com>
2026-02-17 10:44:04 +0800

    created the function that randomly choose between 1-3
    I created a function that randomly choose a number between 1-3 and use switch to make the 1-3 rock, paper or scissor.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getCumputerChoice() {
+    let ranChoice = Math.floor(Math.random() * 3) + 1;
+
+    switch (ranChoice) {
+        case 1:
+            console.log("Rock")
+            break;
+        case 2:
+            console.log("Paper")
+            break;
+        case 3:
+            console.log("Scissor")
+            break;
+    }
+    return ranChoice;
+}
+
+console.log(getCumputerChoice())
