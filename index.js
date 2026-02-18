561d513ef0f85fd3306b7e97076d0c4ebeb47a70
JustinLarry <justinlarrydimaiwat@gmail.com>
2026-02-18 18:04:14 +0800

    Already working but not perfect
    This Simple Rock Paper Scissor game is partially working but can't play multiple rounds yet so just single round for now.

--- index.js
+++ index.js
@@ -3,39 +3,76 @@ let computerScore = 0;
 
 const humanSelection = getHumanChoice();
 const computerSelection = getCumputerChoice();
+const rounds = 5;
 
 function getCumputerChoice() {
     let ranChoice = Math.floor(Math.random() * 3) + 1;
     switch (ranChoice) {
         case 1:
-            console.log("Rock")
+            console.log("Rock");
+            return "Rock";
             break;
         case 2:
-            console.log("Paper")
+            console.log("Paper");
+            return "Paper";
             break;
         case 3:
-            console.log("Scissor")
+            console.log("Scissor");
+            return "Scissor";
             break;
     }
     // return ranChoice;
 }
 
-console.log(getCumputerChoice())
+// console.log(getCumputerChoice())
 
 function getHumanChoice() {
     let humanChoice = prompt("Type Rock, Paper, or Scissor");
+    const upperCasePrompt = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase(1);
 
-    return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase(1);
+    console.log(upperCasePrompt);
+    return upperCasePrompt;
 }
 
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
 
 function playRound(humanChoice, computerChoice) {
     if (computerChoice === "Rock" && humanChoice === "Rock") {
         console.log("DRAW!");
     } else if (computerChoice === "Rock" && humanChoice === "Paper") {
+        console.log("YOU WIN!");
+        humanScore++
+    } else if (computerChoice === "Rock" && humanChoice === "Scissor") {
+        console.log("YOU LOSE")
+        computerScore++
+    } else if (computerChoice === "Paper" && humanChoice === "Rock") {
+        console.log("YOU LOSE")
+        computerScore++
+    } else if (computerChoice === "Paper" && humanChoice === "Paper") {
+        console.log("DRAW")
+    } else if (computerChoice === "Paper" && humanChoice === "Scissor") {
         console.log("YOU WIN!")
+        humanScore++
+    } else if (computerChoice === "Scissor" && humanChoice === "Rock") {
+        console.log("YOU WIN!")
+        humanScore++
+    } else if (computerChoice === "Scissor" && humanChoice === "Paper") {
+        console.log("YOU LOSE")
+        computerScore++
+    } else if (computerChoice === "Scissor" && humanChoice === "Scissor") {
+        console.log("DRAW")
     }
 }
 
-playRound(humanSelection, computerSelection);
+//  playRound(humanSelection, computerSelection)
+
+function playGame() {
+ playRound(humanSelection, computerSelection);
+ console.log("Your Score: " + humanScore);
+ console.log("Computer Score: " + computerScore);
+}
+
+
+for (let i = 1; i <= rounds; i++) {
+    playGame()
+}
